143be3c43968c6435e7128d84c892f6a1c96bf7b
jicoing <jicoing@gmail.com>
2025-12-25 09:33:37 +0000

    chore: switch to gtag

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import ReactGA from 'react-ga4';
-
-// Initialize GA4 — only in production (optional but recommended)
-if (process.env.NODE_ENV === 'production') {
-  ReactGA.initialize('G-0G1F10J5Y4');
-}
 
 createRoot(document.getElementById("root")!).render(<App />);
